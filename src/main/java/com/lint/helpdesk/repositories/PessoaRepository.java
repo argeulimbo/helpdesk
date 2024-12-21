7082a1f1104faaeaf486aa0a0f007869c8161c97
argeulimbo <argeusousaptbr@gmail.com>
2024-12-21 09:36:44 -0300

    Exceção para CPF e Email já cadastrados

--- src/main/java/com/lint/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/lint/helpdesk/repositories/PessoaRepository.java
@@ -1,11 +1,17 @@
 package com.lint.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lint.helpdesk.domain.Pessoa;
+import java.util.List;
+
+
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 	
-	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 
 }
