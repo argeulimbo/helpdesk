2a4508b8d5bbf80324e75227612bc30485fe2d82
argeulimbo <argeusousaptbr@gmail.com>
2024-12-21 00:27:40 -0300

    Criado método Create

--- src/main/java/com/lint/helpdesk/domain/Tecnico.java
+++ src/main/java/com/lint/helpdesk/domain/Tecnico.java
@@ -2,8 +2,10 @@ package com.lint.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lint.helpdesk.domain.dtos.TecnicoDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -22,6 +24,17 @@ public class Tecnico extends Pessoa {
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamado() {
--- src/main/java/com/lint/helpdesk/services/TecnicoService.java
+++ src/main/java/com/lint/helpdesk/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.lint.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lint.helpdesk.domain.Tecnico;
+import com.lint.helpdesk.domain.dtos.TecnicoDTO;
 import com.lint.helpdesk.repositories.TecnicoRepository;
 import com.lint.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -18,6 +20,16 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);	
 	} 
 	
 }
