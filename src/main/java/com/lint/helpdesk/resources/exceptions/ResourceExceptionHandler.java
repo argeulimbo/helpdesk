fbf558e8dba388c1d939412ccb4952de9f19d745
argeulimbo <argeusousaptbr@gmail.com>
2024-12-23 10:21:11 -0300

    Validação nos campos de criação de novo usuário

--- src/main/java/com/lint/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lint/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.lint.helpdesk.resources.exceptions;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,6 +29,19 @@ public class ResourceExceptionHandler {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
 				"Violação de Dados!", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, HttpServletRequest request) {
+		
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Validation Errors!", "Erro na validação dos campos", request.getRequestURI());
+		
+		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 	
 }
