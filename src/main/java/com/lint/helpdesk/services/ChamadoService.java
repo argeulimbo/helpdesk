39d9d37a50a7a109b94ed710c3f6c437abe351a8
argeulimbo <argeusousaptbr@gmail.com>
2024-12-27 16:55:05 -0300

    Update para Chamado implementado

--- src/main/java/com/lint/helpdesk/services/ChamadoService.java
+++ src/main/java/com/lint/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.lint.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,7 +51,9 @@ public class ChamadoService {
 		if(obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
-		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -59,6 +62,13 @@ public class ChamadoService {
 		chamado.setObservacoes(obj.getObservacoes());
 		
 		return chamado;
+	}
+
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
 	}
 	
 }
